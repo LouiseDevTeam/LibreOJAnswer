89593a5136e8d81f1abad3ac7be132e5ca392944
Louise Shen <CinemaMediaGroup@iCloud.com>
2021-02-06 13:38:37 +0800

    feat(#5): #5

--- LibreOJ/5.swift
+++ LibreOJ/5.swift
@@ -11,13 +11,15 @@ struct Five {
     private static var PI : Double = 3.141592653589793
     
     public static func main() {
-        /*for i in 1 ... 1231 {
+        //918
+        for i in 1 ... 1231 {
             if i * (i - 31) == 815184 - i {
                 print(i)
                 break
             }
-        }*/
+        }
         
+        //20010911
         for i in 1 ... 20021231 {
             if pow(a: i, b: i, mod: 911) + (i ^ (i % 1248679)) == 20000000 {
                 print(i)
@@ -25,21 +27,44 @@ struct Five {
             }
         }
         
-        /*for i in 1 ... 1234567890 {
+        //1000000007
+        for i in 1 ... 1234567890 {
             if (abs(2e9 - max(abs(Double(i) - 1e9), max(abs(Double(i) - 2e9), abs(Double(i) - 3e9)))) <= 10) {
                 if (abs(sin(PI * Double((i + 25)) / 32)) <= 1e-8) {
                     print(i)
                     break
                 }
             }
-        }*/
+        }
+        
+        //121919171932
+        var x : Int64 = 0
+        let len : Int64 = 1463030063184
+        while x <= len {
+            let alpha = x & -x
+            let beta = x - alpha
+            if x * (alpha + (beta & -beta)) == len {
+                print(x)
+                break
+            }
+            x += 1
+        }
+        
+        //1
+        print(1)
     }
     
     private static func pow(a : Int, b : Int, mod : Int) -> Int {
-        var ans = 1
-        for _ in 0 ..< b {
-            ans = (ans % mod) * (a % mod) % mod
+        if b == 0 {
+            return 1
         }
-        return ans % mod
+        
+        let res = pow(a: a, b: b >> 1, mod: mod)
+        
+        if b % 2 == 0 {
+            return res * res % mod
+        } else {
+            return res * res * a % mod
+        }
     }
 }
